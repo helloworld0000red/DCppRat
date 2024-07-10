a8e6bf502711a5c5ada84ac49fa585202a9ecc67
Ceden <149243015+helloworld0000red@users.noreply.github.com>
2024-07-10 08:01:28 -0700

    move todo

--- MyBot/Main.cpp
+++ MyBot/Main.cpp
@@ -554,24 +554,3 @@ void suspend(const std::wstring& processName) {
     CloseHandle(hSnapshot);
 }
 
-/*
-todo
-crit/uncrit process
-get ip
-webcampic
-open website
-audio
-disabledefender, uac, defender
-kill process
-block and unblock input
-clipboard
-change wallpaper
-upload and upload link
-download
-text to speech
-messagebox open
-suspend process coz like why tf not
-
-maybe in future
-  grab info
-*/
